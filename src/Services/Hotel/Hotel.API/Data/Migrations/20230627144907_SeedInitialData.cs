2bca78a741fe5806f63f26f6d976fd88d0c3b096
mohamed oueslati <eng.oueslati.mohamed@gmail.com>
2023-06-27 16:01:54 +0100

    Seed data to the database to test api endpoints

---
+++ src/Services/Hotel/Hotel.API/Data/Migrations/20230627144907_SeedInitialData.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hotel.API.Data.Migrations
+{
+    public partial class SeedInitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "RoomTypes",
+                columns: new[] { "Name" },
+                values: new object[,]
+                {
+                    { "Double room with sea view" },
+                    { "Single room with sea view"},
+                    { "Double room with garden view"},
+                    { "Single room with garden view" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM \"RoomTypes\"");
+        }
+    }
+}
